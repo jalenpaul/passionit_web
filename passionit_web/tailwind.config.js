b50a379974442deb92a06eec0d4e20cc12cb1e3f
jalenpaul <38700576+jalenpaul@users.noreply.github.com>
2023-03-23 13:24:27 -0700

    Add files via upload

---
+++ passionit_web/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      'theme' : '#26f2f2',
+      'theme-hover' : '#20c5c5',
+      'darkest' : '#1e1e1e',
+      'backgroundDark' : '#252526',
+      'secondaryDark' : '#2d2d30',
+      'thirdDark' : '#3e3e42',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
